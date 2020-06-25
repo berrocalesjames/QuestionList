7b2a8945ba883230b710293c18a539c195da669e
berrocalesjames <berrocalesjames@gmail.com>
2020-06-25 06:56:57 -0400

    Successfully outputting list to screen.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Ask extends React.Component {
+      constructor(props) {
+          super(props);
+    this.state = {
+        question: '',
+        questionList: [],
+      }
+    }
+    
+      questionHandler = (event) => {
+        event.preventDefault();
+        this.setState({
+          question: event.target.value
+        });
+      };
+
+      handleSubmit = (event) => {
+        event.preventDefault();
+        let newList = this.state.questionList;
+        // newList.push(event.target.value);
+        newList.push(this.state.question);
+        this.setState({
+          questionList: newList,          
+        }, () => localStorage.setItem('questionsList', this.state.questionList))
+      }
+      
+  
+      render () {
+        console.log("Rendering")
+    return (
+      <div>
+        <h1>Questions for Loree</h1>
+        <form value={this.state.question} onSubmit={this.handleSubmit}>      
+            <input
+              type="text"
+              name="question-entry"
+              placeholder="Type question here"
+              value={this.state.question}
+              onChange={this.questionHandler}
+            />
+          <p>Listing as of: <br/>{Date()}</p>
+          <p>Current questions: </p>
+          <ol>
+            {this.state.questionList.map((e, index) => {
+              return <li key={index}>{e}</li>
+          })}
+          </ol>
+        </form>
+        
+      </div>
+
+    )
+  }
 }
 
-export default App;
+export default Ask;
